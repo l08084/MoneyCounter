cd83bdd01a741a4773211d73462669a1ce3a3330
l08084 <l08084@shibaura-it.ac.jp>
2016-05-04 15:26:22 +0900

    履歴ViewController(テンプレート)作成

---
+++ MoneyCounter/MonthHistoryViewController.swift
@@ -0,0 +1,111 @@
+//
+//  MonthHistoryViewController.swift
+//  MoneyCounter
+//
+//  Created by 中安拓也 on 2016/05/04.
+//  Copyright © 2016年 l08084. All rights reserved.
+//
+
+import UIKit
+
+class MonthHistoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private var myTableView: UITableView!
+    private var myButton = UIButton()
+    
+    private var moneyList: [String] = ["1月", "2月", "3月","4月", "5月", "6月",
+                                       "7月", "8月", "9月", "10月", "11月", "12月"]
+    
+    var repo: Repository = Repository()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        
+        // Cell名の登録をおこなう.
+        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DataSourceの設定をする.
+        myTableView.dataSource = self
+        
+        // Delegateを設定する.
+        myTableView.delegate = self
+        
+        // Viewに追加する
+        self.view.addSubview(myTableView)
+        
+        myButton = UIButton()
+        
+        // ボタンを生成する.
+        myButton.frame = CGRectMake(0, 0, 60, 60)
+        myButton.backgroundColor = UIColor.redColor()
+        myButton.setTitle("Back", forState: .Normal)
+        myButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        myButton.layer.masksToBounds = true
+        myButton.layer.cornerRadius = 30.0
+        myButton.layer.position = CGPointMake(self.view.frame.width/2, self.view.frame.height-100)
+        myButton.addTarget(self, action: #selector(ListViewController.onClickMyButton(_:)), forControlEvents: .TouchUpInside)
+        
+        // ボタンを追加する.
+        self.view.addSubview(myButton)
+        /*
+        repo = Repository()
+        
+        let spends = repo.findSpendList()
+        
+        for spend in spends {
+            moneyList.append(spend.spendMoney)
+        }
+ */
+    }
+    
+    /*
+     ボタンのアクション時に設定したメソッド.
+     */
+    internal func onClickMyButton(sender: UIButton){
+        performSegueWithIdentifier("HistoryToMain", sender: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+ 
+    
+    /*
+     Cellが選択された際に呼び出されるデリゲートメソッド.
+     */
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        performSegueWithIdentifier("HistoryToMain", sender: nil)
+    }
+    
+    /*
+     Cellの総数を返すデータソースメソッド.
+     (実装必須)
+     */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moneyList.count
+    }
+    
+    /*
+     Cellに値を設定するデータソースメソッド.
+     (実装必須)
+     */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // 再利用するCellを取得する.
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        
+        // Cellに値を設定する.
+        cell.textLabel!.text = "\(moneyList[indexPath.row])"
+        
+        return cell
+    }
+    
+}
